f73ac9bf5c9f462609528eb9654b3fb1117914eb
Jens Poulsen <jpoulsen@iworq.com>
2024-10-24 13:47:54 -0600

    move components to their own files

---
+++ src/MovieDetails.js
@@ -0,0 +1,142 @@
+import { useState, useEffect } from 'react';
+import { OMDB_KEY } from './App';
+import { Loader } from './Loader';
+import StarRating from './StarRating';
+
+export function MovieDetails({
+  selectedId,
+  onCloseMovie,
+  onAddWatchedMovie,
+  watched,
+}) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [userRating, setUserRating] = useState('');
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRating = watched.find(
+    (movie) => movie.imdbID === selectedId,
+  )?.userRating;
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runTime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  function handleAdd() {
+    const newWatchedMovie = {
+      imdbID: selectedId,
+      title,
+      year,
+      poster,
+      imdbRating: Number(imdbRating),
+      runtime: Number(runTime.split(' ')[0]),
+      userRating,
+    };
+
+    onAddWatchedMovie(newWatchedMovie);
+    onCloseMovie();
+  }
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (e.code === 'Escape') onCloseMovie();
+    };
+
+    document.addEventListener('keydown', callback);
+
+    return () => {
+      document.removeEventListener('keydown', callback);
+    };
+  }, [onCloseMovie]);
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      setIsLoading(true);
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${OMDB_KEY}&i=${selectedId}`,
+      );
+      const data = await res.json();
+      setMovie(data);
+    }
+    getMovieDetails();
+    setIsLoading(false);
+  }, [selectedId]);
+
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return function () {
+      document.title = 'usePopcorn';
+    };
+  }, [title]);
+
+  return (
+    <div className='details'>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className='btn-back' onClick={onCloseMovie}>
+              ←
+            </button>
+            <img src={poster} alt={`Poster of ${movie}`} />
+            <div className='details-overview'>
+              <h2>{title}</h2>
+
+              <p>
+                {released} &bull; {runTime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐️</span>
+                <span>{imdbRating} IMDb rating</span>
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className='rating'>
+              {!isWatched ? (
+                <>
+                  <StarRating
+                    maxRating={10}
+                    size={24}
+                    onSetRating={setUserRating}
+                  />
+                  {userRating > 0 && (
+                    <button className='btn-add' onClick={handleAdd}>
+                      + Add to List
+                    </button>
+                  )}
+                </>
+              ) : (
+                <p>
+                  You rated this movie {watchedUserRating} <span>⭐️</span>
+                </p>
+              )}
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>
+              <strong>Actors:</strong> {actors}
+            </p>
+            <p>
+              <strong>Directed By:</strong> {director}
+            </p>
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
